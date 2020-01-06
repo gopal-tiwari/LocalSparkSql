4aa5bebc17802bd6724942bc1213816e344e8820
Gopal <gopal.tiwari2012@gmail.com>
2020-01-06 21:45:49 +0530

    show database repeated

--- src/main/scala/org/connected/spark/LocalSparkHiveTest.scala
+++ src/main/scala/org/connected/spark/LocalSparkHiveTest.scala
@@ -3,6 +3,7 @@ package org.connected.spark
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 
 object LocalSparkHiveTest {
+
   def main(args: Array[String]): Unit = {
 
     val spark: SparkSession = SparkSession.builder()
@@ -14,7 +15,19 @@ object LocalSparkHiveTest {
     spark.sql("SHOW DATABASES").show
     spark.sql("CREATE DATABASE IF NOT EXISTS sparkdemo")
     spark.sql("CREATE TABLE IF NOT EXISTS sparkdemo.table1(id INT, name STRING)")
+    spark.sql("SHOW DATABASES").show
 
+    import spark.implicits._
+    val df:DataFrame = Seq(
+      (1, "One"),
+      (2, "Two"),
+      (3, "Three")
+    ).toDF("id","name")
+
+    df.write.mode(SaveMode.Append).format("hive").saveAsTable("sparkdemo.table1")
+    //Thread.sleep(60 * 1000)
+    spark.sql("SELECT * FROM sparkdemo.table1").show(false)
+    println(spark.sql("select * from sparkdemo.table1").count)
 
   }
 }
